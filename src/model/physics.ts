54fdce48585b3cb0e250ef693f9b3cc9af1b811d
Mikael Pajunen <mikael.pajunen@gmail.com>
2019-06-09 15:39:18 +0300

    Add basic motion model and calculations

---
+++ src/model/physics.ts
@@ -0,0 +1,35 @@
+export type Vector = {
+  x: number
+  y: number
+}
+
+export type Position = { p: Vector }
+
+export type Velocity = { v: Vector }
+
+export type Acceleration = { a: Vector }
+
+export type Motion = Position & Velocity & Acceleration
+
+export const zero: Vector = { x: 0, y: 0 }
+
+export const add = (v1: Vector, v2: Vector): Vector => ({ x: v1.x + v2.x, y: v1.y + v2.y })
+
+export const sum = (vectors: Vector[]): Vector => vectors.reduce(add, zero)
+
+export const updateVelocity = ({ a }: Acceleration, { v }: Velocity): Velocity => ({ v: add(a, v) })
+
+export const updatePosition = ({ v }: Velocity, { p }: Position): Position => ({ p: add(v, p) })
+
+export const updateMotion = (motion: Motion, acceleration: Acceleration): Motion => {
+  const velocity = updateVelocity(acceleration, motion)
+  const position = updatePosition(velocity, motion)
+
+  return { p: position.p, v: velocity.v, a: acceleration.a }
+}
+
+export const restingAt = (vector: Vector): Motion => ({
+  p: vector,
+  v: zero,
+  a: zero,
+})

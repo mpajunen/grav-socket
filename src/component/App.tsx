d57d6caf822052f924f4fd54167844d60ec0b5c7
Mikael Pajunen <mikael.pajunen@gmail.com>
2019-06-09 15:40:11 +0300

    Show Game component

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import './App.css'
+import Game from './Game'
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        Grav Socket
-      </header>
+      <Game />
     </div>
   )
 }
